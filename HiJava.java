e47a493df6ff258c655457269389c29b9b84bc70
8826427394 <navneet.kumar@kritikalvision.ai>
2023-03-01 15:35:12 +0530

    Added hii java file

---
+++ HiJava.java
@@ -0,0 +1,13 @@
+public class HelloJava {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hii Java  ///");
+		System.out.println("hello is changed to hii");
+		  
+		  
+	
+
+	}
+
+}
